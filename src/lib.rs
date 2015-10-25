f0f23ccdd12c5eb978ada045d40f1b80087d78da
David Judd <david@academia.edu>
2015-10-25 11:02:25 -0700

    Add first piece of real parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,56 @@
+extern crate regex;
+
+#[macro_use]
+extern crate lazy_static;
+
+mod suffix;
+
 use std::option;
+use regex::Regex;
 
 pub struct Name {
   raw: String,
   pub given_name: String,
   pub surname: String,
-  pub middle_initials: String,
+  pub middle_names: String,
+}
+
+fn is_nickname(word: &str) -> bool {
+    // TODO
+    false
 }
 
 impl Name {
-    pub fn new(name: &str) -> Option<Name> {
-        let words: Vec<&str> = name.split_whitespace().collect();
+    pub fn parse(name: &str) -> Option<Name> {
+        let mut words: Vec<&str> = Vec::new();
+
+        // Strip suffixes and nicknames, and then flip remaining words around
+        // (remaining) comma (for formats like "Smith, John"; but suffixes may
+        // also contain commas, e.g. "John Smith, esq.")
+        for part in name.rsplit(',') {
+            // TODO Skip suffix check for first part
+            // TODO Skip nickname check for first & last words
+            if !suffix::is_suffix(part) {
+                for word in part.split_whitespace() {
+                    if !is_nickname(word) {
+                        words.push(word);
+                    }
+                }
+            }
+        }
+
+        // We need at least a first and last name, or we can't tell which we have
         if words.len() < 2 {
             return None;
         }
+
+        // TODO Check if last word is suffix
 
         let parsed = Name {
             raw: name.to_string(),
             given_name: words[0].to_string(),
             surname: words[1..].join(" "),
-            middle_initials: "".to_string(), // TODO
+            middle_names: "".to_string(), // TODO
         };
         return Some(parsed);
     }
@@ -26,4 +58,5 @@ impl Name {
     pub fn display(&self) -> String {
         return format!("{} {}", self.given_name, self.surname);
     }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 extern crate human_name;
 
 fn main() {
-    let name = human_name::Name::new("Emma Goldman").unwrap();
-    println!("{}", name.display());
+    let names = [
+        "Emma Goldman", 
+        "Emma ('Em') Goldman", 
+        "Emma \"the anarchist\" Goldman", 
+        "Emma Goldman, M.D.", 
+        "Emma Goldman, esq", 
+        "Goldman, Emma",
+    ];
+
+    for name in names.iter() {
+        let name = human_name::Name::parse(name).unwrap();
+        println!("{}", name.display());
+    }
 }
