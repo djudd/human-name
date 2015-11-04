333a6c7b419bb8835fb2f53db22165f44d7c805d
David Judd <david@academia.edu>
2015-11-04 10:46:20 -0800

    Move benchmark code into module

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@
 extern crate human_name;
 extern crate test;
 
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::fs::File;
-use test::Bencher;
-
 fn debug(names: Vec<&str>) {
     for raw in names.iter() {
         let maybe_name = human_name::Name::parse(raw);
@@ -34,58 +29,68 @@ fn main() {
 
 }
 
-#[bench]
-fn bench_parsing_first_last(b: &mut Bencher) {
-    b.iter(|| {
-        let parsed = human_name::Name::parse("Juan Garcia");
-        test::black_box(parsed.is_none())
-    })
-}
+#[cfg(test)]
+mod bench {
+    use std::io::prelude::*;
+    use std::io::BufReader;
+    use std::fs::File;
+    use test::Bencher;
+    use test::black_box;
+    use human_name::Name;
 
-#[bench]
-fn bench_parsing_sort_order(b: &mut Bencher) {
-    b.iter(|| {
-        let parsed = human_name::Name::parse("Garcia, J.Q.");
-        test::black_box(parsed.is_none())
-    })
-}
+    #[bench]
+    fn bench_parsing_first_last(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed = Name::parse("Juan Garcia");
+            black_box(parsed.is_none())
+        })
+    }
 
-#[bench]
-fn bench_parsing_unparseable(b: &mut Bencher) {
-    b.iter(|| {
-        let parsed = human_name::Name::parse("foo@bar.com");
-        test::black_box(parsed.is_none())
-    })
-}
+    #[bench]
+    fn bench_parsing_sort_order(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed = Name::parse("Garcia, J.Q.");
+            black_box(parsed.is_none())
+        })
+    }
 
-#[bench]
-fn bench_parsing_complex(b: &mut Bencher) {
-    b.iter(|| {
-        let parsed = human_name::Name::parse("鈴木 Velasquez y Garcia, Dr. Juan Q. 'Don Juan' Xavier III");
-        test::black_box(parsed.is_none())
-    })
-}
+    #[bench]
+    fn bench_parsing_unparseable(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed = Name::parse("foo@bar.com");
+            black_box(parsed.is_none())
+        })
+    }
 
-#[bench]
-fn bench_parsing_many(b: &mut Bencher) {
-    let f = File::open("tests/benchmark-names.txt").ok().unwrap();
-    let reader = BufReader::new(f);
-    let names: Vec<String> = reader.lines().map( |l| l.ok().unwrap() ).collect();
+    #[bench]
+    fn bench_parsing_complex(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed = Name::parse("鈴木 Velasquez y Garcia, Dr. Juan Q. 'Don Juan' Xavier III");
+            black_box(parsed.is_none())
+        })
+    }
 
-    b.iter(move || {
-        let mut valid = 0;
-        let mut invalid = 0;
+    #[bench]
+    fn bench_parsing_many(b: &mut Bencher) {
+        let f = File::open("tests/benchmark-names.txt").ok().unwrap();
+        let reader = BufReader::new(f);
+        let names: Vec<String> = reader.lines().map( |l| l.ok().unwrap() ).collect();
 
-        for name in names.iter() {
-            let parsed = human_name::Name::parse(&name);
-            if parsed.is_none() {
-                invalid += 1;
-            } else {
-                valid += 1;
+        b.iter(move || {
+            let mut valid = 0;
+            let mut invalid = 0;
+
+            for name in names.iter() {
+                let parsed = Name::parse(&name);
+                if parsed.is_none() {
+                    invalid += 1;
+                } else {
+                    valid += 1;
+                }
             }
-        }
 
-        test::black_box(valid);
-        test::black_box(invalid);
-    })
+            black_box(valid);
+            black_box(invalid);
+        })
+    }
 }
