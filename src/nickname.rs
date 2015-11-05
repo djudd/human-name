21f9cbe5947a987376c7f4634fd33f20a82af930
David Judd <david@academia.edu>
2015-11-04 19:45:17 -0800

    More consistent ambiguous/unambigious nickname indicator handling

--- src/nickname.rs
+++ src/nickname.rs
@@ -1,13 +1,14 @@
 use std::borrow::Cow;
 
-fn expected_close_char_if_opens_nickname(c: char, prev: char) -> Option<char> {
+// Returns tuple (close_char, must_precede_whitespace)
+fn expected_close_char_if_opens_nickname(c: char, follows_whitespace: bool) -> Option<(char, bool)> {
     let close = match c {
-        '(' => Some(')'),
-        '[' => Some(']'),
-        '<' => Some('>'),
-        '“' => Some('”'),
-        '〝' => Some('〞'),
-        '‹' => Some('›'),
+        '(' => Some((')', false)),
+        '[' => Some((']', false)),
+        '<' => Some(('>', false)),
+        '“' => Some(('”', false)),
+        '〝' => Some(('〞', false)),
+        '‹' => Some(('›', false)),
         _ => None
     };
 
@@ -17,14 +18,14 @@ fn expected_close_char_if_opens_nickname(c: char, prev: char) -> Option<char> {
         return close;
     }
 
-    if prev.is_whitespace() {
+    if follows_whitespace {
         // Treat, e.g., quote character as the start of a nickname
         // only if it occurs after whitespace; otherwise, it
         // might be in-name puntuation
         match c {
-            '\'' => Some('\''),
-            '"' => Some('"'),
-            '‘' => Some('’'),
+            '\'' => Some(('\'', true)),
+            '"' => Some(('"', true)),
+            '‘' => Some(('’', true)),
             _ => None
         }
     } else {
@@ -49,19 +50,26 @@ pub fn second_char_index(s: &str) -> Option<usize> {
 pub fn strip_nickname(input: &str) -> Cow<str> {
     let mut nick_start_ix = 0; // This counts as not-found; we won't classify the whole string as a nickname
     let mut expected_close_char = '\0';
+    let mut must_precede_whitespace = false;
     let mut prev_char = '\0';
 
     for (i,c) in input.char_indices() {
         if nick_start_ix == 0 {
-            let close = expected_close_char_if_opens_nickname(c, prev_char);
+            let close = expected_close_char_if_opens_nickname(c, prev_char.is_whitespace());
             if !close.is_none() {
                 nick_start_ix = i;
-                expected_close_char = close.unwrap();
+                expected_close_char = close.unwrap().0;
+                must_precede_whitespace = close.unwrap().1
             }
         } else if c == expected_close_char {
             match second_char_index(&input[i..]) {
                 Some(j) => {
-                    return Cow::Owned(input[0..nick_start_ix].to_string() + " " + &strip_nickname(&input[i+j..]));
+                    if input[i+j..].chars().nth(0).unwrap().is_whitespace() || !must_precede_whitespace {
+                        return Cow::Owned(input[0..nick_start_ix].to_string() + " " + &strip_nickname(&input[i+j..]));
+                    }
+                    else {
+                        return Cow::Owned(input[0..i].to_string() + &strip_nickname(&input[i..]));
+                    }
                 }
                 None => {
                     return Cow::Borrowed(&input[0..nick_start_ix]);
@@ -73,8 +81,8 @@ pub fn strip_nickname(input: &str) -> Cow<str> {
     }
 
     if nick_start_ix > 0 {
-        if expected_close_char == ')' {
-            // When there's an opening parens, but no closing parens, strip the
+        if !must_precede_whitespace {
+            // When there's, e.g., an opening parens, but no closing parens, strip the
             // rest of the string
             return Cow::Borrowed(&input[0..nick_start_ix]);
         } else { match second_char_index(&input[nick_start_ix..]) {
