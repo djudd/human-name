38b8f2c5ef39ef940fd18379ad68dca12ca3ab67
David Judd <david@academia.edu>
2015-11-23 12:03:31 -0800

    Add docs for to_json

--- src/serialization.rs
+++ src/serialization.rs
@@ -3,6 +3,24 @@ use std::collections::BTreeMap;
 use super::Name;
 
 impl ToJson for Name {
+
+    /// Serializes a name into parsed components.
+    ///
+    /// ```
+    /// # extern crate rustc_serialize;
+    /// # extern crate human_name;
+    /// #
+    /// use human_name::Name;
+    /// use rustc_serialize::json::ToJson;
+    ///
+    /// # fn main() {
+    /// let name = Name::parse("JOHN ALLEN Q MACDONALD JR").unwrap();
+    /// assert_eq!(
+    ///   r#"{"first_initial":"J","given_name":"John","middle_initial":"AQ","middle_names":"Allen","suffix":"Jr.","surname":"MacDonald"}"#,
+    ///   name.to_json().to_string()
+    /// );
+    /// # }
+    /// ```
     fn to_json(&self) -> Json {
         let mut d = BTreeMap::new();
         d.insert("surname".to_string(), self.surname().to_json());
