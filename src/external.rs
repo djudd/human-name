d612107806170f57bc4659ccb9480aa85f2a1d95
David Judd <david@academia.edu>
2015-12-03 15:46:52 -0800

    DRY-up external.rs with macros

--- src/external.rs
+++ src/external.rs
@@ -7,6 +7,25 @@ use std::ptr;
 use std::hash::{Hash,Hasher,SipHasher};
 use super::Name;
 
+macro_rules! str_to_char_star {
+    ($str:expr) => { {
+        let s = CString::new($str).unwrap();
+        s.into_raw()
+    } }
+}
+
+macro_rules! option_str_to_char_star {
+    ($opt:expr) => {
+        match $opt {
+            Some(string) => {
+                let s = CString::new(string).unwrap();
+                s.into_raw()
+            }
+            None => ptr::null()
+        }
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn human_name_parse(input: *const libc::c_char) -> Option<Box<Name>> {
     let s = unsafe {
@@ -43,42 +62,27 @@ pub extern "C" fn human_name_hash(name: &Name) -> u64 {
 
 #[no_mangle]
 pub extern "C" fn human_name_surname(name: &Name) -> *const c_char {
-    let s = CString::new(name.surname().into_owned()).unwrap();
-    s.into_raw()
+    str_to_char_star!(name.surname().into_owned())
 }
 
 #[no_mangle]
 pub extern "C" fn human_name_given_name(name: &Name) -> *const c_char {
-    match name.given_name() {
-        Some(given_name) => {
-            let s = CString::new(given_name).unwrap();
-            s.into_raw()
-        }
-        None => ptr::null()
-    }
+    option_str_to_char_star!(name.given_name())
 }
 
 #[no_mangle]
 pub extern "C" fn human_name_initials(name: &Name) -> *const c_char {
-    let s = CString::new(name.initials()).unwrap();
-    s.into_raw()
+    str_to_char_star!(name.initials())
 }
 
 #[no_mangle]
 pub extern "C" fn human_name_first_initial(name: &Name) -> *const c_char {
-    let s = CString::new(name.first_initial().to_string()).unwrap();
-    s.into_raw()
+    str_to_char_star!(name.first_initial().to_string())
 }
 
 #[no_mangle]
 pub extern "C" fn human_name_middle_initials(name: &Name) -> *const c_char {
-    match name.middle_initials() {
-        Some(initials) => {
-            let s = CString::new(initials).unwrap();
-            s.into_raw()
-        }
-        None => ptr::null()
-    }
+    option_str_to_char_star!(name.middle_initials())
 }
 
 #[no_mangle]
@@ -88,34 +92,20 @@ pub extern "C" fn human_name_goes_by_middle_name(name: &Name) -> bool {
 
 #[no_mangle]
 pub extern "C" fn human_name_middle_names(name: &Name) -> *const c_char {
-    match name.middle_name() {
-        Some(middle_names) => {
-            let s = CString::new(middle_names.into_owned()).unwrap();
-            s.into_raw()
-        }
-        None => ptr::null()
-    }
+    option_str_to_char_star!(name.middle_name().map(|n| n.into_owned()))
 }
 
 #[no_mangle]
 pub extern "C" fn human_name_suffix(name: &Name) -> *const c_char {
-    match name.suffix() {
-        Some(suffix) => {
-            let s = CString::new(suffix).unwrap();
-            s.into_raw()
-        }
-        None => ptr::null()
-    }
+    option_str_to_char_star!(name.suffix())
 }
 
 #[no_mangle]
 pub extern "C" fn human_name_display_short(name: &Name) -> *const c_char {
-    let s = CString::new(name.display_short()).unwrap();
-    s.into_raw()
+    str_to_char_star!(name.display_short())
 }
 
 #[no_mangle]
 pub extern "C" fn human_name_display_full(name: &Name) -> *const c_char {
-    let s = CString::new(name.display_full()).unwrap();
-    s.into_raw()
+    str_to_char_star!(name.display_full())
 }
